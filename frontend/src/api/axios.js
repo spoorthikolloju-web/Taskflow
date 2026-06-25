82f3ed6e2c8d283cb0306e39f138fcb6ecdd26a8
Spoorthi <spoorthikolloju@gmail.com>
2026-06-25 15:37:25 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } })
+const api = axios.create({ baseURL: 'https://taskflow-16we.onrender.com/api', headers: { 'Content-Type': 'application/json' } })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
